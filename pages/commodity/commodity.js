58d6e37a006aebc3d95983f1e9ac1fbfe3a2aed4
xuyongkang <320859852@qq.com>
2018-05-15 11:08:22 +0800

    商品列表对接

--- pages/commodity/commodity.js
+++ pages/commodity/commodity.js
@@ -1,9 +1,41 @@
 //commodity.js
 //获取应用实例
 const app = getApp()
+var url = app.IP +"chatGoods/listPageGoods";
+var page = 1;
 
+var GetList = function (that) {
+  that.setData({
+    hidden: false
+  });
+  wx.request({
+    url: url,
+    data: {
+      pageSize: 10,
+      pageNo: page
+    },
+    success: function (res) {
+      console.log(res.data);
+      var l = that.data.list
+      for (var i = 0; i < res.data.goodslist.length; i++) {
+        l.push(res.data[i])
+      }
+      that.setData({
+        list: l
+      });
+      page++;
+    }
+  });
+  that.setData({
+    hidden: true
+  });
+} 
 Page({
   data: {
+    hidden: true,
+    list: [],
+    scrollTop: 0,
+    scrollHeight: 0  ,
     selected: true,
     selected1: false,
     selected2: false,
@@ -15,7 +47,37 @@ Page({
     commodity_li_right_width: wx.getSystemInfoSync().windowWidth * 0.88 - 100
   },
   onLoad: function () {
+    var that = this;
+    wx.getSystemInfo({
+      success: function (res) {
+        console.info(res.windowHeight);
+        that.setData({
+          scrollHeight: res.windowHeight
+        });
+      }
+    });
+  }, 
+  onShow: function () {
+    var that = this;
+    GetList(that);
   },
+  bindDownLoad: function () {
+    var that = this;
+    GetList(that);
+  },
+  scroll: function (event) {
+    this.setData({
+      scrollTop: event.detail.scrollTop
+    });
+  },
+  refresh: function (event) {
+    page = 1;
+    this.setData({
+      list: [],
+      scrollTop: 0
+    });
+    GetList(this)
+  },  
   select: function (e) {
     this.setData({
       selected1: false,
@@ -64,5 +126,13 @@ Page({
       selected2: false,
       selected3: true
     })
+  },
+  //下拉
+  onPullDownRefresh: function(e){
+    console.log("123");
+  },
+
+  onReachBottom:function(e) {
+    console.log("321")
   }
 })
