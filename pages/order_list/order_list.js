459507da5b58f06946e778a42e61bb5150a407d3
haodaxiang <1539433181@qq.com>
2018-05-16 15:31:40 +0800

    下单页数量加减（没加js）和到店自取按钮、新增订单列表页

---
+++ pages/order_list/order_list.js
@@ -0,0 +1,116 @@
+// pages/order_list/order_list.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    selected: true,
+    selected1: false,
+    selected2: false,
+    selected3: false,
+    selected4: false,
+    commodity_li_right_width: wx.getSystemInfoSync().windowWidth * 0.88 - 80,
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  },
+  selected: function (e) {
+    this.setData({
+      selected1: false,
+      selected2: false,
+      selected3: false,
+      selected4: false,
+      selected: true
+    })
+  },
+  selected1: function (e) {
+    this.setData({
+      selected: false,
+      selected2: false,
+      selected3: false,
+      selected4: false,
+      selected1: true
+    })
+  },
+  selected2: function (e) {
+    this.setData({
+      selected: false,
+      selected1: false,
+      selected3: false,
+      selected2: true,
+      selected4: false,
+    })
+  },
+  selected3: function (e) {
+    this.setData({
+      selected: false,
+      selected1: false,
+      selected2: false,
+      selected3: true,
+      selected4: false,
+    })
+  },
+  selected4: function (e) {
+    this.setData({
+      selected: false,
+      selected1: false,
+      selected2: false,
+      selected3: false,
+      selected4: true,
+    })
+  }
+})
