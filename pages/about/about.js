80753d835fe1f36b1f4357e7a7021316da8a45b1
1432920449 <1432920449@qq.com>
2018-06-02 18:07:39 +0800

    关于我们

--- pages/about/about.js
+++ pages/about/about.js
@@ -1,66 +1,90 @@
 // pages/about/about.js
+var header = getApp().globalData.header;
+var WxParse = require('../wxParse/wxParse.js');
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    //选择组件对象
+    this.toast = this.selectComponent("#toast");
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+    var that = this;
+    wx.request({
+      url: getApp().IP + 'chatAboutus/aboutus',
+      method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      header: header, // 设置请求的 header
+      dataType: 'json',
+      responseType: 'text',
+      success: function (res) {
+        // success
+        WxParse.wxParse('text', 'html', res.data.aboutus.CONTENT, that, 5);
+      },
+      fail: function () {
+        // fail
+        setTimeout(function () {
+          that.toast.showView("加载失败");
+        }, 100)
+      },
+      complete: function () {
+        // complete
+        // wx.hideToast();
+      }
+    });
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
